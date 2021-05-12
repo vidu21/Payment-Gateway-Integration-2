052ba560cbdc7cbdd137ccd155a58147bab0e2fa
Varun <54104000+varunrana8@users.noreply.github.com>
2021-05-12 19:58:49 +0530

    Update info.php

--- info.php
+++ info.php
@@ -10,8 +10,7 @@ if (empty($name) || empty($email) || empty($message))
 else
 {
     mail("varunrana8@gmail.com", "PTE NGO MESSAGE", $message, "From: $name < $email > ");
-    echo "<script type='text/javascript'>alert('Message sent Successfully');
-    window.history.go(-1);
+    echo "<script type='text/javascript'>alert('Message sent Successfully')
     </script>";
 }
 ?>
