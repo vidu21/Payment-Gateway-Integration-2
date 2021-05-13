9bd3b2cb984743e476b0c0670b3904cde223a721
Varun <54104000+varunrana8@users.noreply.github.com>
2021-05-13 17:06:07 +0530

    Update info.php

--- info.php
+++ info.php
@@ -1,16 +1,19 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['Email'];
-$message = $_POST['Message'];
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['Email'];
+    $message = $_POST['Message'];
+    
+    if (empty($name) || empty($email) || empty($message))
+    {
+        echo "Please Fill all the details";
+    }
+    else
+    {
+        mail("varunrana8@gmail.com", "PTE NGO MESSAGE", $message, "From: $name < $email > ");
+        echo "<script type='text/javascript'>alert('Message sent Successfully')
+        </script>";
+    }
+}
 
-if (empty($name) || empty($email) || empty($message))
-{
-    echo "Please Fill all the details";
-}
-else
-{
-    mail("varunrana8@gmail.com", "PTE NGO MESSAGE", $message, "From: $name < $email > ");
-    echo "<script type='text/javascript'>alert('Message sent Successfully')
-    </script>";
-}
 ?>
